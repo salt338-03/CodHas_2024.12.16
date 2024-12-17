688626afbe7d26dc3347a87e6c97bb9ad3d46094
salt338-03 <salt338@naver.com>
2024-12-17 12:15:18 +0900

    수정

--- CodHas_2024.12.16/Form1.cs
+++ CodHas_2024.12.16/Form1.cs
@@ -17,14 +17,14 @@ namespace CodHas_2024._12._16
             InitializeComponent();
 
             string[] ArrayPractice = new string[10];
+            string[] input = "이름, 나이, 전화번호".Split(',');
 
             ArrayPractice[0] = "동해 물과 백두산이".IndexOf("백두산이").ToString();
             ArrayPractice[1] = "토요일에 먹는 토마토".LastIndexOf("토").ToString();
             ArrayPractice[2] = "질서 있는 퇴장".Contains("퇴").ToString();
-            ArrayPractice[3] = "그 사람의 그림자는 그랬다".Replace("그", "이").ToString();
-            ArrayPractice[4] = "삼성 갤럭시".Insert(3, "애플").ToString();
-            ArrayPractice[5] = "오늘은 왠지 더 배고프다".Remove(7, 1).ToString();
-            string[] input = "이름, 나이, 전화번호".Split(',');
+            ArrayPractice[3] = "그 사람의 그림자는 그랬다".Replace("그", "이");
+            ArrayPractice[4] = "삼성 갤럭시".Insert(3, "애플");
+            ArrayPractice[5] = "오늘은 왠지 더 배고프다".Remove(7, 1);
             ArrayPractice[6] = input[0];
             ArrayPractice[7] = input[1];
             ArrayPractice[8] = input[2];
