61b1afdd5e94eb1b460ddc455ea15554216ed1fa
salt338-03 <salt338@naver.com>
2024-12-18 10:39:32 +0900

    함수실습

--- CodHas_2024.12.16/Form1.cs
+++ CodHas_2024.12.16/Form1.cs
@@ -93,9 +93,9 @@ namespace CodHas_2024._12._16
 
             int divisor = 2;
             int dividend = 11;
-            int[] Result = CalculateQuotientAndRemainder(divisor, dividend);
+            int[] Result = CalculateQuotientNRemainder(divisor, dividend);
 
-             int[] CalculateQuotientAndRemainder(int x, int y)
+             int[] CalculateQuotientNRemainder(int x, int y)
             {
                 int[] numbers = new int[2]; 
                 numbers[0] = y / x; // 몫
